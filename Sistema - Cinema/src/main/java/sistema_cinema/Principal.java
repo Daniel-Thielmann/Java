37a69105a69d7a97c6f411dd022090436e7b2cf3
Daniel Thielmann <thielmanndan@gmail.com>
2024-08-15 17:41:58 -0300

    new changes

--- Sistema - Cinema/src/main/java/sistema_cinema/Principal.java
+++ Sistema - Cinema/src/main/java/sistema_cinema/Principal.java
@@ -1,8 +1,9 @@
 import java.util.Date;
 
+//Aluno: Daniel Alves Thielmann - 202165020AB
+
 public class Principal {
     public static void main(String[] args) {
-        // Criação de objetos
         Genero genero = new Genero("Ação");
 
         Ator ator1 = new Ator("Chris Evans", "Protagonista", 40, 'M', "Americana");
@@ -27,18 +28,26 @@ public class Principal {
         Ingresso ingresso1 = new Ingresso(1, 10, "Inteira", 50.0f, "2024-08-12 16:00");
         Ingresso ingresso2 = new Ingresso(2, 15, "Meia-entrada", 25.0f, "2024-08-12 16:10");
 
-        // Exibir sessão antes da venda
+        
+        System.out.println("ANTES DA VENDA");
         sessao.exibirSessao();
+        System.out.println("\n");
 
-        // Venda de ingressos
+
+        System.out.println("VENDEU INGRESSOS?");
         sessao.venderIngresso(ingresso1);
         sessao.venderIngresso(ingresso2);
+        System.out.println("\n");
 
-        // Exibir sessão após a venda
+
+        System.out.println("APÓS A VENDA");
         sessao.exibirSessao();
+        System.out.println("\n");
 
-        // Imprimir ingressos
+
+        System.out.println("IMPRIMIR INGRESSOS");
         ingresso1.imprimirIngresso();
+        System.out.println("\n");
         ingresso2.imprimirIngresso();
     }
 }
